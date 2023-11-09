25671ab97bea521cef9a8e476a0c6f5aa9774384
archivoccp@gmail.com <archivoccp@gmail.com>
2023-11-09 10:16:59 -0500

    block

--- js/reloj.js
+++ js/reloj.js
@@ -1,27 +1,27 @@
 // Reloj
-document.addEventListener('DOMContentLoaded', function() {
-    const timeElement = document.getElementById('time');
-    const dateElement = document.getElementById('date');
-    const mesElement = document.getElementById('mes');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const timeElement = document.getElementById('time');
+//     const dateElement = document.getElementById('date');
+//     const mesElement = document.getElementById('mes');
   
   
-    function updateTime() {
-        const now = new Date();
+//     function updateTime() {
+//         const now = new Date();
   
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        const seconds = String(now.getSeconds()).padStart(2, '0');
+//         const hours = String(now.getHours()).padStart(2, '0');
+//         const minutes = String(now.getMinutes()).padStart(2, '0');
+//         const seconds = String(now.getSeconds()).padStart(2, '0');
   
-        const timeString = `${hours}:${minutes}:${seconds}`;
-        timeElement.textContent = timeString;
+//         const timeString = `${hours}:${minutes}:${seconds}`;
+//         timeElement.textContent = timeString;
   
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        const dateString = now.toLocaleDateString(undefined, options);
-        const mesActual = new Intl.DateTimeFormat(undefined, { month: 'long' }).format(now);
-        dateElement.textContent = dateString;
-        mesElement.textContent = mesActual;
-    }
+//         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+//         const dateString = now.toLocaleDateString(undefined, options);
+//         const mesActual = new Intl.DateTimeFormat(undefined, { month: 'long' }).format(now);
+//         dateElement.textContent = dateString;
+//         mesElement.textContent = mesActual;
+//     }
   
-    updateTime();
-    setInterval(updateTime, 1000);
-  });
+//     updateTime();
+//     setInterval(updateTime, 1000);
+//   });
--- js/script.js
+++ js/script.js
@@ -1,89 +1,23 @@
-// Llamado de API Facebook
-// window.fbAsyncInit = function() {
-//   FB.init({
-//       appId            : '256877673892471',
-//       autoLogAppEvents : true,
-//       xfbml            : true,
-//       version          : 'v17.0'
-//       });
 
-//       FB.api(
-//           '/me',
-//           'GET',
-//           {"fields": "id,name,picture{url}"},
-//           function(response) {
-              
-//           }
-//       );
-// };
-
-// Banner para mensajes 
-
-document.addEventListener("DOMContentLoaded", function () {
-  const mensajeElement = document.querySelector(".message-text");
-  const footerContainer = document.querySelector(".mensajes");
-
-  let messages = [
-    //Limite de caracteres por frase 120
-    "Agendate con la Cámara de Comercio y consulta los beneficios de estar formalizado en www.camarapamplona.org.co",
-    "Recuerda nuestro horario de atención     Lunes a Viernes    de 8:00am - 12:00pm    y    de 2:00pm - 6:00pm",
-    "Nuestros asesores estan aquí para brindarte la información que necesitas",
-    "La generación de oportunidades de emprendimiento, fortalecimiento y aceleración empresarial de la región, son nuestro compromiso",
-    "Siguenos en nuestras redes sociales Facebook, Instagram y Twitter como # Camara de Comercio de Pamplona",
-    "Prestador de Servicios Turisticios-Alojamientos, no olvides alimentar día a día tu Tarjeta de Registro de Alojamiento",
-    "Recuerda nuestros canales de contacto: ccpamplona@camarapamplona.org.co, teléfonos (60)75680993 - 75684696 - 75682047 y WhatsApp: 333 033 3569",
-    "Recuerda que el término de ley para atender tus tramites es de QUINCE (15) DÍAS HÁBILES",
-    "No olvides calificar el servicio prestado por nuestro personal, pregunta con nuestras Asesoras como hacerlo",
-    "Además de tu registro en Cámara de Comercio, adquieres responsabilidades tributarias en la DIAN y en la Alcaldía Municipal  (Impuesto de Industria y Comercio).",
-    "Si tienes actividades de alto impacto, debes gestionar los permisos necesarios ante las autoridades locales (Planeación municipal, sayco y acinpro, bomberos, entre otros).",
-    "Recuerda que puedes realizar tu matrícula mercantil mediante la Ventanilla Unica Empresarial VUE, aplica solo para Pamplona."
-  ];
-
-  const mensajeWidth = mensajeElement.offsetWidth;
-  const containerWidth = footerContainer.offsetWidth;
-
-  let currentMessageIndex = 0;
-
-  const updateMessage = () => {
-    mensajeElement.textContent = messages[currentMessageIndex];
-    currentMessageIndex = (currentMessageIndex + 1) % messages.length;
-    
-    gsap.set(mensajeElement, { x: containerWidth }); 
-    gsap.to(mensajeElement, {
-      x: -mensajeWidth, 
-      duration: (containerWidth) * 0.03,
-      ease: "linear",
-      onComplete: updateMessage 
-    });
-  };
-
-  updateMessage();
-
-});
-
-
-  
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   const obtenerValorButton = document.getElementById("guardar");
-//   const mensajeElement = document.querySelector("#texto");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const mensajeElement = document.querySelector(".message-text");
 //   const footerContainer = document.querySelector(".mensajes");
-//   const texto = document.getElementById("prueba");
 
-//   obtenerValorButton.addEventListener("click", function() {
-//     console.log("Botón Guardar clickeado");
-
-//     Recorre los inputs para capturar los valores y actualizar messages
-//     const inputElements = document.querySelectorAll("#prueba");
-//     inputElements.forEach((input, index) => {
-//       const valorIngresado = input.value;
-//       messages[index] = valorIngresado;
-//     });
-
-//     console.log(messages);
-//   });
-
-//   let messages = [];
+//   let messages = [
+//     //Limite de caracteres por frase 120
+//     "Agendate con la Cámara de Comercio y consulta los beneficios de estar formalizado en www.camarapamplona.org.co",
+//     "Recuerda nuestro horario de atención     Lunes a Viernes    de 8:00am - 12:00pm    y    de 2:00pm - 6:00pm",
+//     "Nuestros asesores estan aquí para brindarte la información que necesitas",
+//     "La generación de oportunidades de emprendimiento, fortalecimiento y aceleración empresarial de la región, son nuestro compromiso",
+//     "Siguenos en nuestras redes sociales Facebook, Instagram y Twitter como # Camara de Comercio de Pamplona",
+//     "Prestador de Servicios Turisticios-Alojamientos, no olvides alimentar día a día tu Tarjeta de Registro de Alojamiento",
+//     "Recuerda nuestros canales de contacto: ccpamplona@camarapamplona.org.co, teléfonos (60)75680993 - 75684696 - 75682047 y WhatsApp: 333 033 3569",
+//     "Recuerda que el término de ley para atender tus tramites es de QUINCE (15) DÍAS HÁBILES",
+//     "No olvides calificar el servicio prestado por nuestro personal, pregunta con nuestras Asesoras como hacerlo",
+//     "Además de tu registro en Cámara de Comercio, adquieres responsabilidades tributarias en la DIAN y en la Alcaldía Municipal  (Impuesto de Industria y Comercio).",
+//     "Si tienes actividades de alto impacto, debes gestionar los permisos necesarios ante las autoridades locales (Planeación municipal, sayco y acinpro, bomberos, entre otros).",
+//     "Recuerda que puedes realizar tu matrícula mercantil mediante la Ventanilla Unica Empresarial VUE, aplica solo para Pamplona."
+//   ];
 
 //   const mensajeWidth = mensajeElement.offsetWidth;
 //   const containerWidth = footerContainer.offsetWidth;
@@ -93,18 +27,17 @@ document.addEventListener("DOMContentLoaded", function () {
 //   const updateMessage = () => {
 //     mensajeElement.textContent = messages[currentMessageIndex];
 //     currentMessageIndex = (currentMessageIndex + 1) % messages.length;
-
-//     gsap.set(mensajeElement, { x: containerWidth });
+    
+//     gsap.set(mensajeElement, { x: containerWidth }); 
 //     gsap.to(mensajeElement, {
-//       x: -mensajeWidth,
-//       duration: containerWidth * 0.02,
+//       x: -mensajeWidth, 
+//       duration: (containerWidth) * 0.03,
 //       ease: "linear",
-//       onComplete: updateMessage,
+//       onComplete: updateMessage 
 //     });
 //   };
 
 //   updateMessage();
 
-  
 // });
 
